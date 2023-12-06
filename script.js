0a3ca43c8e7d92f6fd783ad3ba7a9e79e1c76ee6
Piotr Kosiński <piograammer@gmail.com>
2023-12-06 21:01:03 +0100

    Create about-me section

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const menuBtn = document.querySelector('.menu-btn')
-const closeBtn = document.querySelector('.close-menu-btn')
+const closeMenuBtn = document.querySelector('.close-menu-btn')
+const links = document.querySelector('.links')
+const media = window.matchMedia('(min-width: 992px)')
+
+
+const checkMedia = (media) => {
+    if (media.matches) {
+        links.classList.remove('active')
+        menuBtn.style="display:none"
+        closeMenuBtn.style="display:none"
+    } else {
+        menuBtn.style="display:block"
+    }
+}
 
 const showMenu = () => {
-    menuBtn.style="display:none";
-    closeBtn.style="display:block";
-    
+    links.classList.add('active')
+    menuBtn.style="display:none"
+    closeMenuBtn.style="display: block"
+}
+
+const closeMenu = () => {
+    links.classList.remove('active')
+    closeMenuBtn.style="display: none"
+    menuBtn.style="display: block"
 }
 
 menuBtn.addEventListener('click', showMenu)
+closeMenuBtn.addEventListener('click', closeMenu)
+media.addEventListener('change', ()=>{checkMedia(media)})
