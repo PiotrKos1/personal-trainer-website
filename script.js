5cf29d74a6560217f78880139fc893769aeecb9a
Piotr Kosiński <piograammer@gmail.com>
2023-12-10 21:53:27 +0100

    Repair JS code, add mediaMax

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-const menuBtn = document.querySelector('.menu-btn')
-const closeMenuBtn = document.querySelector('.close-menu-btn')
-const links = document.querySelector('.links')
-const media = window.matchMedia('(min-width: 992px)')
+const menuBtn = document.querySelector(".menu-btn");
+const closeMenuBtn = document.querySelector(".close-menu-btn");
+const links = document.querySelector(".links");
+const mediaMin = window.matchMedia("(min-width: 992px)");
+const mediaMax = window.matchMedia("(max-width: 992px)");
 
 // właściwość matchMedia - wywołanie zdarzenia na zmianę rozdzielczości
 
-const checkMedia = (media) => {
-    if (media.matches) {
-        links.classList.remove('active')
-        menuBtn.style="display:none"
-        closeMenuBtn.style="display:none"
-    } else {
-        menuBtn.style="display:block"
-    }
-}
+const checkMedia = mediaMin => {
+  if (mediaMin.matches) {
+    links.classList.remove("active");
+    menuBtn.style = "display:none";
+    closeMenuBtn.style = "display:none";
+  } else {
+    menuBtn.style = "display:block";
+  }
+};
 
 const showMenu = () => {
-    links.classList.add('active')
-    menuBtn.style="display:none"
-    closeMenuBtn.style="display: block"
-}
+  links.classList.add("active");
+  menuBtn.style = "display:none";
+  closeMenuBtn.style = "display: block";
+};
 
-// delegacja zdarzeń, target na jeden przycisk
+const closeMenu = event => {
+  if (event.target.classList.value !== "bi bi-list" && mediaMax.matches) {
+    links.classList.remove("active");
+    closeMenuBtn.style = "display: none";
+    menuBtn.style = "display: block";
+  }
+};
 
-const closeMenu = (event) => {
-    if (event.target.classList.value !== "bi bi-list") {
-    links.classList.remove('active')
-    closeMenuBtn.style="display: none"
-    menuBtn.style="display: block"
-    }
-}
-
-
-menuBtn.addEventListener('click', showMenu)
-media.addEventListener('change', ()=>{checkMedia(media)})
-window.addEventListener('click', closeMenu)
+menuBtn.addEventListener("click", showMenu);
+mediaMin.addEventListener("change", () => {
+  checkMedia(mediaMin);
+});
+window.addEventListener("click", closeMenu);
